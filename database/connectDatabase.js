a04801512162b8a70e1d38ac29c81babceb788ea
Dakshay Bishnoi <dakshaybishnoi@gmail.com>
2023-05-22 13:03:29 +0530

    node version

--- database/connectDatabase.js
+++ database/connectDatabase.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose'
 
 export const connectdb = function(){
-    mongoose.connect("mongodb://localhost:27017",{
+    mongoose.connect(process.env.DB_URI,{
         dbName:"todobackend"
-    }).then(()=>{
-        console.log("database connected .");
+    }).then((c)=>{
+        console.log(`database connected ${c.connection.host}`);
     }).catch((e)=>{
         console.log(e);
     })
